e79524cf30fbb14bafb0eb7f7d693bbedf98b168
PKGzGMTH <peakungg@gmail.com>
2021-05-20 23:26:12 +0700

    just little update :v

--- auto plot.py
+++ auto plot.py
@@ -17,7 +17,28 @@ class bcolors:
 main_command = 'start C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -NoExit C:\\Users\\' + (os.getlogin()) + '\\AppData\\Local\\chia-blockchain\\app-1.1.5\\resources\\app.asar.unpacked\\daemon\\chia'
 temp_list = []
 
-print('How much you want to plot in same time?')
+print('How many Threads of CPU do you have?')
+threads_total = int(input())
+
+print('How many Tempolary drive you have?')
+temp_total = int(input())
+
+for i in range(temp_total):
+    print('\nPlease Enter loaction of Tempolary Directory drive ' + str(i+1))
+    temp_dir = str(input())
+    print('Please Enter capacity of drive ' + str(i+1))
+    temp_count = int(int(input())/257)
+
+    print('\nLocation of Tempolary directory ' + str(i+1) + ' is \"' + temp_dir + '\"  and total temp is : ' + str(temp_count))
+    print('Please enter \'Confirm!\' to confirm this.\n')
+    if input() != 'Confirm!':
+        print('Skip this!')
+        break
+
+    i+1
+
+
+'''
 plot_range = int(input())
 
 for i in range(plot_range):
@@ -35,6 +56,7 @@ final_dir = str(input())
 for i in range(plot_range):
     plot_command = main_command + ' plots create -k 32 -e -b 4000 -r 2 -t ' + temp_list[i] + ' -d ' + final_dir
     print(plot_command)
-    subprocess.call(plot_command, shell=True)
+    #subprocess.call(plot_command, shell=True)
     i+= 1
     time.sleep(900)
+'''
