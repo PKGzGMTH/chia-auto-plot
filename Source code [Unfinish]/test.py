822d772ad8a0c895903bb0683b6d638fc15f3f87
PKGzGMTH <peakungg@gmail.com>
2021-05-31 19:53:59 +0700

    test log and sort Final dir

--- Source code [Unfinish]/test.py
+++ Source code [Unfinish]/test.py
@@ -1,3 +1,54 @@
-import subprocess
+# print status FAIL
+'''
+print('[' + '█'*50 + ']')
+input('press key')
 
-subprocess.call("start cmd.exe",shell=True)
+import time
+
+print("          Wryyyyyyyyyyyyyyyyyy", end="\r")
+time.sleep(2)
+print('zaworldo')
+
+
+import sys,time
+print("FAILED...") 
+print("FAILED...") 
+print("FAILED...") 
+sys.stdout.write("\033[F") #back to previous line
+sys.stdout.write("\033[K") #clear line 
+sys.stdout.write("\033[F") #back to previous line
+sys.stdout.write("\033[K") #clear line 
+sys.stdout.write("\033[F") #back to previous line
+sys.stdout.write("\033[K") #clear line 
+time.sleep(2)
+print("SUCCESS!")
+'''
+
+# read log line(?) FAIL
+'''
+f = open('D:\.github\chia-auto-plot\Source code [Unfinish]\Chia-Auto-Plot-27-05-21T22-44-18.txt','r')
+log = f.read()
+f.close()
+line_count = (log.count('\n') + 1)
+print(line_count)
+'''
+
+# list final directory unFinish
+stop_list = []
+final_list = []
+final_dir = ['a','b','c']
+final_count = [4,5,3]
+stop_count = 0
+for i in range(max(final_count)):
+    for i in range(len(final_count)):
+        if final_count[i] > 0:
+            final_list += final_dir[i]
+            final_count[i] = final_count[i] - 1
+            stop_count+=1
+    stop_list.append(stop_count)
+
+print(final_list)
+print(stop_list)
+
+for i in stop_list:
+    print(final_list[i-1])
